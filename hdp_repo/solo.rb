993fcaed16d5567ff18c9ccdac0e01150917e2f2
David Streever <dstreever@hortonworks.com>
2014-01-02 14:29:40 -0500

    Testing solo implementation.

---
+++ hdp_repo/solo.rb
@@ -0,0 +1,8 @@
+root = File.absolute_path(File.dirname(__FILE__))
+
+file_cache_path  root
+cookbook_path    root + '/cookbooks'
+
+log_level        :info
+log_location     STDOUT
+ssl_verify_mode  :verify_none
